689540f5dff9b330fdf4946a882c95e5f20c3c4e
jawell <skytor08@gmail.com>
2018-12-21 19:24:45 +0200

    add core components for loading

--- loader.php
+++ loader.php
@@ -3,3 +3,5 @@
 require_once 'application/core/autoload.php';
 require_once 'application/core/Paths.php';
 require_once 'application/core/Router.php';
+require_once 'application/core/Controller.php';
+require_once 'application/core/View.php';
